996e6ed9f81b5121662d6925d3f205178f8c343b
URL-Admin <zea3dhyperx@gmail.com>
2025-11-01 16:21:46 +0530

    healthcare added.

---
+++ app/education/page.tsx
@@ -0,0 +1,22 @@
+// "use client";
+// import React from "react";
+// import { Footer } from "@/components/footer";
+// // import { Header } from "@/components/header";
+
+// export default function WhyUsPage() {
+//   return (
+//     <main className="min-h-screen">
+//       <Header />
+//       <iframe
+//         src="/industries/Educationservice/index.html"
+//         title="Education & Training"
+//         style={{
+//           width: "100%",
+//           height: "calc(100vh - 0px)", // adjust for header/footer height
+//           border: "none",
+//         }}
+//       />
+//       <Footer />
+//     </main>
+//   );
+// }
